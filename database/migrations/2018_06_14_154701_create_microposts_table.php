64e59d18ec33ee80e7de257d2c4b7f6454275039
junya.ariyoshi <ariiin1083@gmail.com>
2018-06-20 04:26:30 +0000

    lastchallenge

--- database/migrations/2018_06_14_154701_create_microposts_table.php
+++ database/migrations/2018_06_14_154701_create_microposts_table.php
@@ -15,7 +15,11 @@ class CreateMicropostsTable extends Migration
     {
         Schema::create('microposts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('content');
             $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
